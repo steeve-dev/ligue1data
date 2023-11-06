9591c7864721455f343887775e8d169f9adf3f59
SteeveMellet <s.mellet@consolweb.com>
2023-11-06 21:48:50 +0100

    continue

--- ligue1data/requests.py
+++ ligue1data/requests.py
@@ -1,4 +1,5 @@
 import http.client
+import json
 
 def getLigue1():
     conn = http.client.HTTPSConnection("api-football-v1.p.rapidapi.com")
@@ -13,13 +14,20 @@ def getLigue1():
     print(data.decode("utf-8"))
 
 
-def getLigue1Teams():
+def getLigue1Teams(id):
     conn = http.client.HTTPSConnection("api-football-v1.p.rapidapi.com")
     headers = {
         'X-RapidAPI-Key': "08b693ac18msh71caadac3647d76p1d887fjsn29e7dc24331f",
         'X-RapidAPI-Host': "api-football-v1.p.rapidapi.com"
     }
-    conn.request("GET", "/v3/standings?season=2023&league=61", headers=headers)
+    conn.request("GET", f"/v3/teams/statistics?league=61&season=2023&team={id}", headers=headers)
     res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
+    data = res.read().decode("utf-8")
+    response = json.loads(data)
+    team = response.get("response", {}).get("team", [])
+    goals = response.get("response", {}).get("total", [])
+    return team
+
+
+
+
--- ligue1data/views.py
+++ ligue1data/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from ligue1data.requests import getLigue1Teams
 
 
 def index(request):
-    return render(request, "index.html")
+    team_ids = [84, 85, 91, 93, 79, 106, 111, 83, 81, 116, 94, 95, 82, 96, 97, 112, 99, 80]
+    teams_data = []
+    for team_id in team_ids:
+        team_data = getLigue1Teams(team_id)
+        teams_data.append(team_data)
+    return render(request, "index.html", context={"teams_data": teams_data})
